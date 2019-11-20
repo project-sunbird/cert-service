7a8b8e420a2f0b85ba1b3727af4777de1afd283d
Harikumar Palemkota <harip@ilimi.in>
2019-11-20 17:31:19 +0530

    Issue #SC-1418 added itext license for supporting glyphs in pdf generation

--- certgen/certProcessor/src/main/java/org/incredible/certProcessor/views/PdfConverter.java
+++ certgen/certProcessor/src/main/java/org/incredible/certProcessor/views/PdfConverter.java
@@ -1,10 +1,16 @@
 package org.incredible.certProcessor.views;
 
 import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.licensekey.LicenseKey;
+import org.incredible.certProcessor.JsonKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class PdfConverter {
 
@@ -13,6 +19,12 @@ public class PdfConverter {
     public static void convertor(File htmlSource, String certUuid, String directory) {
         File file = new File(directory, certUuid + ".pdf");
         try {
+            boolean licenseEnabled = Boolean.parseBoolean(System.getenv(JsonKey.ITEXT_LICENSE_ENABLED));
+            if(licenseEnabled) {
+                String licensePath = System.getenv(JsonKey.ITEXT_LICENSE_PATH);
+                InputStream ip = PdfConverter.class.getResourceAsStream(licensePath);
+                LicenseKey.loadLicenseFile(ip);
+            }
             HtmlConverter.convertToPdf(htmlSource, file);
             logger.info("Pdf file is created ");
         } catch (FileNotFoundException e) {
