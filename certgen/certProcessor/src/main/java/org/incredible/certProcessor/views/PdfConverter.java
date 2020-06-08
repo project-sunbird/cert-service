24ad1e66d77f0138a0fa89349bb019feac176f89
aishwa8141 <a.aish1997@gmail.com>
2020-06-08 18:09:31 +0530

    added batik library to convert svg to pdf

--- certgen/certProcessor/src/main/java/org/incredible/certProcessor/views/PdfConverter.java
+++ certgen/certProcessor/src/main/java/org/incredible/certProcessor/views/PdfConverter.java
@@ -2,6 +2,7 @@ package org.incredible.certProcessor.views;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.io.File;
 
 
@@ -9,15 +10,30 @@ public class PdfConverter {
 
     private static Logger logger = LoggerFactory.getLogger(PdfConverter.class);
 
-    public static void convertor(File htmlSource, String certUuid, String directory) {
+    public static void convertor(File sourceFile, String certUuid, String directory) {
         File file = new File(directory, certUuid + ".pdf");
+//        File file = new File(directory, certUuid + "cert.png");
+//        File file = new File(directory, certUuid + ".jpeg");
+
         try {
             //html to pdf convertion using headLess chrome
-            HeadlessChromeHtmlToPdfConverter.convert(htmlSource, file);
+            HeadlessChromeHtmlToPdfConverter.convert(sourceFile, file);
+
             //using Itext
-//            ItextHtmlToPdfConverter.convert(htmlSource,file);
+//            ItextHtmlToPdfConverter.convert(sourceFile,file);
+
+            //svg to pdf
+//            BatikSvgToPdfConverter.convert(sourceFile.getAbsolutePath(), file);
+
+            //svg to png
+//            BatikSvgToPngConverter.convert(sourceFile.getAbsolutePath(), file);
+
+            //svg to jpeg
+//            BatikSvgToJpegConverter.convert(sourceFile.getAbsolutePath(), file);
+
+
             logger.info("Pdf file is created for the {} ", certUuid);
-        }catch (Exception e) {
+        } catch (Exception e) {
             logger.error("exception while generating pdf file {}", e.getMessage());
         }
     }
